8818dacb32fa7fd7ec2be8efdd9f002c358fe302
Benis Tambe <will.jenifer@yahoo.com>
2022-01-23 22:52:38 -0500

    added footer

---
+++ club-showcase-react-fontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          <small>&copy; 2022 Benis USA</small>
+        </p>
+      </footer>
+    );
+  }
+}
