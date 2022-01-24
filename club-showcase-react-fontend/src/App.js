36aa799a88d4493d86c00d7c6a136af3c5199f10
Benis Tambe <will.jenifer@yahoo.com>
2022-01-23 22:50:54 -0500

    navigation and app mount

--- club-showcase-react-fontend/src/App.js
+++ club-showcase-react-fontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Navigation from "./components/Navigation.js";
 class App extends Component {
   render() {
     return (
+            <Navigation />
     );
   }
 }
--- club-showcase-react-fontend/src/index.js
+++ club-showcase-react-fontend/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
+    <App />,
   document.getElementById("root")
 );
 
