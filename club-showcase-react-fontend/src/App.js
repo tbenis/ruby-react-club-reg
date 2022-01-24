e6c700e1ab0aeaa742ad43c3e4891600f1bc932b
Benis Tambe <will.jenifer@yahoo.com>
2022-01-23 23:02:53 -0500

    Imported club components into App

--- club-showcase-react-fontend/src/App.js
+++ club-showcase-react-fontend/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Navigation from "./components/Navigation.js";
+
+import ClubsList from "./components/ClubsList";
+import ClubNew from "./components/ClubNew";
+import Footer from "./components/Footer";
+import Club from "./components/Club";
 class App extends Component {
   render() {
     return (
