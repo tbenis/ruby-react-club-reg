2fef38d7af0b879257f24954fb749aa8cdf497f7
Benis Tambe <will.jenifer@yahoo.com>
2022-01-23 22:53:02 -0500

    added about component

---
+++ club-showcase-react-fontend/src/components/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class About extends Component {
+
+
+    render() {
+        return (
+            <>
+                About me 
+            
+            
+            </>
+
+        )
+    }
+
+}
