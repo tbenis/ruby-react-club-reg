2e919f47a064115e8c1c2e42a07aec331394645d
Benis Tambe <will.jenifer@yahoo.com>
2022-01-24 07:55:37 -0500

    backend validations

--- club-registration-api/app/models/club.rb
+++ club-registration-api/app/models/club.rb
@@ -1,4 +1,16 @@
 class Club < ApplicationRecord
   has_one_attached :logo
+  # validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  # validates :date_founded, presence: true
+  # validates :summary, presence: true
+  # validate :date_founded_cannot_be_in_the_future
+  # validates :description, length: { minimum: 10 }
 
+  scope :alphabeticallize, -> { order(:name) }
+
+  def date_founded_cannot_be_in_the_future
+    if date_founded.present? && date_founded > Date.today
+      errors.add(:date_founded, "Cannot be in the future")
+    end
+  end
 end
