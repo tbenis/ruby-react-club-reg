ceb50e3cb24846e0bf19a7c480fe5f9c64f3816b
Benis Tambe <will.jenifer@yahoo.com>
2022-01-25 10:01:17 -0500

    added  class

--- club-showcase-react-fontend/src/components/Navigation.js
+++ club-showcase-react-fontend/src/components/Navigation.js
@@ -5,6 +5,8 @@ export default class Navigation extends React.Component {
     render () {
         return(
             <nav className="nav">
+                <a className="clubsLink navLink" href="http://localhost:4000/" style={{textAlign: "left"}}>Clubs</a>
+                <a className="createClubLink navLink" href="http://localhost:4000/clubs/new" style={{textAlign: "right"}}>Create Club</a>
                 <a className="aboutLink navLink" href="http://localhost:4000/about" style={{textAlign: "right"}}>About</a>
             </nav>
         )
